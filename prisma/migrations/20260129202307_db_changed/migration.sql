61f1f3be1f7eb68a5ec2d325b76dc4a44e4a0988
Arijit <arijitbasubd@gmail.com>
2026-01-30 04:33:12 +0600

    error boundry added in app.ts and schema.prisma updated

---
+++ prisma/migrations/20260129202307_db_changed/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `dosage` on the `Medicine` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Medicine" DROP COLUMN "dosage";
--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,8 @@ import { toNodeHandler } from "better-auth/node";
 import { authMiddleware } from "./middlewares/auth";
 import { UserRole } from "../generated/prisma/enums";
 import { notFound } from "./middlewares/notFound";
+import { medicineRouter } from "./modules/medicine/medicine.router";
+import { categoryRouter } from "./modules/category/category.router";
 const app: Application = express();
 app.all("/api/auth/*splat", toNodeHandler(auth));
 
@@ -21,6 +23,9 @@ app.use(express.json());
 //   res.send("Admin access");
 // });
 
+app.use("/medicines", medicineRouter)
+app.use("/categories", categoryRouter)
+
 app.get("/", (req: Request, res: Response) => {
   res.status(200).send({
     success: true,
@@ -30,4 +35,12 @@ app.get("/", (req: Request, res: Response) => {
 });
 
 app.use(notFound)
+app.use((err: any, req: Request, res: Response, next: any) => {
+  console.error(err);
+  res.status(err?.statusCode || 500).json({
+    success: false,
+    message: err?.message || "Internal server error",
+    data: null,
+  });
+});
 export default app;
